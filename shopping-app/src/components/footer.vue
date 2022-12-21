c4b112d994d9a67a38728b2ed1551264defa9c7e
King <kingsjacobfrancis@gmail.com>
2022-12-21 16:16:48 +0100

    footer update

--- shopping-app/src/components/footer.vue
+++ shopping-app/src/components/footer.vue
@@ -1,14 +1,19 @@
 <script>
-   export default {
-    
-   }
+    export default {
+        computed:{
+          getYear(){
+            const date = new Date()
+            return date.getFullYear()
+          }
+        }
+    }
 </script>
 
 <template>
     <div class="flex justify-center items-center mb-10 text-[14px] text-center">
       <div>
         <p>Made with ❤️ and Vue JS</p>
-        <p>&copy; 2022, codeCrown12</p>
+        <p>&copy; {{ getYear }} | codeCrown12</p>
       </div>
     </div>
 </template>
