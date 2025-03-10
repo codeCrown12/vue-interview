89e56b3ffca77f98c5c1a99e5b62e40249565edc
King Jacob <72860575+codeCrown12@users.noreply.github.com>
2025-03-10 08:52:28 +0100

    Update footer.vue

--- shopping-app/src/components/footer.vue
+++ shopping-app/src/components/footer.vue
@@ -13,7 +13,7 @@
     <div class="flex justify-center items-center mb-10 text-[14px] text-center">
       <div>
         <p>Made with ❤️ and React JS</p>
-        <p>&copy; {{ getYear }} | codeCrown12</p>
+        <p>&copy; {{ getYear }}</p>
       </div>
     </div>
 </template>
